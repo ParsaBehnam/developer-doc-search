811b6bab73935c2a55d18c10d0c282dff97867b8
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-05-11 12:04:54 +0300

    add data/ to .gitigonre

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from web_crawler import WebCrawler
+from db import create_database, insert_into_article
 
 def main():
+    create_database()
+
     wc = WebCrawler('https://developer.mozilla.org/en-US/docs/Web', 1)
-    print(wc.crawl())
 
+    data = wc.crawl()
 
-   
-
+    for url, article in data.items():
+        insert_into_article(url, article['title'], article['content'])
 
 if __name__ == "__main__":
     main()

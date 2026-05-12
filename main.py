2416bc8c1ac6edc229691f24d048310f0b494273
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-05-13 01:10:12 +0300

    add testing for get_articles() in main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from web_crawler import WebCrawler
 from db import create_database, insert_into_articles
+from search.loader import get_articles
 
 def main():
-    create_database()
+    # create_database()
 
-    wc = WebCrawler('https://developer.mozilla.org/en-US/docs/Web', 1)
+    # wc = WebCrawler('https://developer.mozilla.org/en-US/docs/Web', 1)
 
-    data = wc.crawl()
+    # data = wc.crawl()
 
-    for url, article in data.items():
-        insert_into_articles(url, article['title'], article['content'])
+    # for url, article in data.items():
+    #     insert_into_articles(url, article['title'], article['content'])
+
+    articles = get_articles()
+    print(len(articles))
+    print(articles[0][1])
 
 if __name__ == "__main__":
     main()
